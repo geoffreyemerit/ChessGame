d6f8b896baf5416d309d296cdbeee5431bc44e68
Geoffrey Emerit <geoffrey.emerit@gmail.com>
2022-08-31 19:30:16 +0200

    branch chessboard created ans positionned in the center

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import React from "react";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+
+import ChessBoard from "./components/ChessBoard";
+
 function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-3xl font-bold text-blue-600">
-        Install & Setup Vite + React + Typescript + Tailwind CSS 3
-      </h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="*" element={<ChessBoard />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/ChessBoard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ChessBoard = () => {
+  return (
+    <div className="bg-[#e8e5e2] w-screen h-screen flex justify-center items-center">
+      <div className="w-[600px] h-[600px] bg-red-600"></div>
+    </div>
+  );
+};
+
+export default ChessBoard;
