297b1855e0116ceaa36e49598cdae979f6a84398
Geoffrey Emerit <geoffrey.emerit@gmail.com>
2022-08-31 19:18:35 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Install & Setup Vite + React + Typescript + Tailwind CSS 3
+      </h1>
+    </div>
+  );
+}
+
+export default App;
